e3819810457fe1262e183103a2b2e33c9de1b249
admalla <maasovec@gmail.com>
2024-08-06 11:39:00 +0300

    branch change

--- src/pages/messenger/ui/Messenger.tsx
+++ src/pages/messenger/ui/Messenger.tsx
@@ -1,7 +1,12 @@
+import dynamic from 'next/dynamic'
+
 import { getHeaderWithSidebarLayout } from '@/widgets/layouts'
 
+// eslint-disable-next-line import/no-unresolved
+const MessengerApp = dynamic(() => import('messengerApp/MessengerBlock'), { ssr: false })
+
 function Messenger() {
-  return <div>Messenger</div>
+  return <MessengerApp />
 }
 
 Messenger.getLayout = getHeaderWithSidebarLayout
---
+++ types/module-fed.d.ts
@@ -0,0 +1,1 @@
+declare module 'messengerApp/MessengerBlock'
